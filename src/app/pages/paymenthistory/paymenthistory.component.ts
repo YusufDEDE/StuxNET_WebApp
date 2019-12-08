915cad0e054185f16d84aa0bbc7272a2dd5d6697
FurkanAlpEsen <efurkan1453@gmail.com>
2019-12-08 22:44:44 +0300

    payment screen

--- src/app/pages/paymenthistory/paymenthistory.component.ts
+++ src/app/pages/paymenthistory/paymenthistory.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/utils/services/app.service';
+import axios from 'axios';
 
 @Component({
   selector: 'app-paymenthistory',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paymenthistory.component.scss']
 })
 export class PaymenthistoryComponent implements OnInit {
+  
+  url:string = 'https://stuxnetapi.herokuapp.com';
+  paymentHistory:any [] = [];
+  tc:any;
 
-  constructor() { }
+  constructor(
+    public appService: AppService,
+    private authenticationService: AppService
+  ) { }
 
   ngOnInit() {
+    const currentUser = this.authenticationService.currentUserValue;
+    this.tc = localStorage.getItem("tc");
+    console.log("tc", this.tc);
+    this.getPayHistory(currentUser.token, this.tc);
+  }
+
+  getPayHistory(token, tc) {
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+    }
+
+    axios.post(this.url+'/api/payment/listofpaymenttransactions',
+      bodyParameters,
+      config
+    ).then((response) => {
+      this.paymentHistory = response.data;
+      console.log("paymenthistory_> ", this.paymentHistory);
+    }).catch((error) => {
+      console.log(error)
+    });
   }
 
 }
