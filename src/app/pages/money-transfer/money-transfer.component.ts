f02c8a766a815d83472c9f67144baf99a2b80ac8
FurkanAlpEsen <efurkan1453@gmail.com>
2019-12-08 21:48:00 +0300

    Updated

--- src/app/pages/money-transfer/money-transfer.component.ts
+++ src/app/pages/money-transfer/money-transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/utils/services/app.service';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import axios from 'axios';
 
 @Component({
   selector: 'app-money-transfer',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoneyTransferComponent implements OnInit {
 
-  constructor() { }
+  tc: any;
+  moneyTransfer: any[] = [];
+
+  constructor(
+    public appService: AppService,
+    private authenticationService: AppService) { }
 
   ngOnInit() {
+    const currentUser = this.authenticationService.currentUserValue;
+    this.tc = localStorage.getItem("tc");
+    this.getMoneyTransfer(currentUser.token, this.tc);
   }
+
+  getMoneyTransfer(token, tcs) {
+    var config = {
+      headers: {'token': "" + token}
+    };
+    
+    var bodyParameters = {
+      tc: parseInt(tcs),
+    }
+    
+    axios.post( 
+      'https://stuxnetapi.herokuapp.com/api/account/listofmoneytransfers ',
+      bodyParameters,
+      config
+    ).then((response) => {
+      this.moneyTransfer = response.data.recordset;
+      console.log("test _>", response, 'win', this.moneyTransfer); // bitince uçur burayı
+    }).catch((error) => {
+      console.log(error)
+    });
+    console.log(this.moneyTransfer);
+  }
+
 
 }
