b8e4e6ad5fb5b7856cccc2e9e4964153bd2f0eef
FurkanAlpEsen <efurkan1453@gmail.com>
2019-12-09 01:48:41 +0300

    NavToolUpdated

--- src/app/pages/virman/virman.component.ts
+++ src/app/pages/virman/virman.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/utils/services/app.service';
+import axios from 'axios';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-virman',
@@ -7,9 +11,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirmanComponent implements OnInit {
 
-  constructor() { }
+  url:string = 'https://stuxnetapi.herokuapp.com';
+  success:any;
+  tc:any;
+  additNo:number;
+  virman:any;
+  accounts:any [] = [];
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public appService: AppService,
+    private authenticationService: AppService,
+    private alertService: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      tc: ['', Validators.required],
+      pw: ['', Validators.required]
+     });
+
+    const currentUser = this.authenticationService.currentUserValue;
+    this.tc = localStorage.getItem("tc");
+    //this.depositAdd(currentUser.token, this.tc, 1017, 100);
+    this.getAccount(currentUser.token, this.tc);
   }
+
+  get f() { return this.loginForm.controls; } 
+
+  getAccountInfo(additNo:number, deposit:number){
+    console.log(additNo, deposit);
+  }
+
+  getAccount(token, tc) {
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+    }
+
+    axios.post(this.url+'/api/account',
+      bodyParameters,
+      config
+    ).then((response) => {
+      this.accounts = response.data;
+      console.log("account_> ", this.accounts);
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
+  virmanAdd(token, tc, additNo, virman) {
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+      additNo: parseInt(additNo),
+      virman: parseFloat(virman),
+    }
+    axios.post(this.url+'/api/account/deposit',
+      bodyParameters,
+      config
+    ).then((response) => {
+      
+      this.success = response.data.recordset[0];
+      if(!this.success || parseInt(virman) < 1){
+        this.alertService.error("Virman işlemi başarısız!");
+      }else{
+        this.alertService.success("Virman işlemi başarılı!");
+      }
+      console.log("virman_> ", this.success);
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
 
 }
--- src/app/pages/withdraw/withdraw.component.ts
+++ src/app/pages/withdraw/withdraw.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/utils/services/app.service';
+import axios from 'axios';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-withdraw',
@@ -7,9 +12,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WithdrawComponent implements OnInit {
 
-  constructor() { }
+  url:string = 'https://stuxnetapi.herokuapp.com';
+  success:any;
+  tc:any;
+  additNo:number;
+  withDraw:any;
+  accounts:any [] = [];
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public appService: AppService,
+    private authenticationService: AppService,
+    private alertService: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      tc: ['', Validators.required],
+      pw: ['', Validators.required]
+     });
+
+    const currentUser = this.authenticationService.currentUserValue;
+    this.tc = localStorage.getItem("tc");
+    //this.depositAdd(currentUser.token, this.tc, 1017, 100);
+    this.getAccount(currentUser.token, this.tc);
+  }
+
+  get f() { return this.loginForm.controls; } 
+
+  getAccountInfo(additNo:number, withdraw:number){
+    console.log(additNo, withdraw);
+  }
+
+  getAccount(token, tc) {
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+    }
+
+    axios.post(this.url+'/api/account',
+      bodyParameters,
+      config
+    ).then((response) => {
+      this.accounts = response.data;
+      console.log("account_> ", this.accounts);
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
+  withDrawAdd(token, tc, additNo, withdraw) {
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+      additNo: parseInt(additNo),
+      withdraw: parseFloat(withdraw),
+    }
+    axios.post(this.url+'/api/account/withdraw',
+      bodyParameters,
+      config
+    ).then((response) => {
+      
+      this.success = response.data.recordset[0];
+      if(!this.success || parseInt(withdraw) < 1){
+        this.alertService.error("Para çekme işlemi başarısız!");
+      }else{
+        this.alertService.success("Para yatırma işlemi başarılı!");
+      }
+      console.log("withdraw_> ", this.success);
+    }).catch((error) => {
+      console.log(error)
+    });
   }
 
 }
