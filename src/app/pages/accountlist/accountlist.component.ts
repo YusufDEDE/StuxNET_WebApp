ab384c0985ab8491778a32c3d731bb7d3cabfb12
Yusuf Dede <yusufdede95@hotmail.com>
2019-12-09 23:31:40 +0300

    open/close account api connect

--- src/app/pages/accountlist/accountlist.component.ts
+++ src/app/pages/accountlist/accountlist.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/utils/services/app.service';
 import axios from 'axios';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accountlist',
@@ -13,9 +15,12 @@ export class AccountlistComponent implements OnInit {
   accounts:any [] = [];
   tc:any;
   
+  
   constructor(
     public appService: AppService,
-    private authenticationService: AppService
+    private authenticationService: AppService,
+    private alertService: ToastrService,
+    private router:Router
   ) { }
 
   ngOnInit() {
@@ -23,6 +28,17 @@ export class AccountlistComponent implements OnInit {
     this.tc = localStorage.getItem("tc");
     console.log("tc", this.tc);
     this.getAccount(currentUser.token, this.tc);
+  }
+
+  newAccOnclik(){
+    const currentUser = this.authenticationService.currentUserValue;
+    this.newAccount(currentUser.token, this.tc);
+  }
+
+  delAccOnclik(additNo) {
+    console.log("burası del");
+    const currentUser = this.authenticationService.currentUserValue;
+    this.removeAccount(currentUser.token, this.tc, additNo);
   }
 
   getAccount(token, tc) {
@@ -39,6 +55,56 @@ export class AccountlistComponent implements OnInit {
     ).then((response) => {
       this.accounts = response.data;
       console.log("account_> ", this.accounts);
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
+  newAccount(token, tc) {
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+    }
+    axios.post(this.url+'/api/account/newAccount',
+      bodyParameters,
+      config
+    ).then((response) => {
+      status = response.data.recordset[0];
+      if(status){
+        this.alertService.success("Hesap Açma işlemi başarılı!");
+        ;
+      } else{
+        this.alertService.error("Hesap Açma işlemi başarısız!");
+      }
+      this.router.navigate(['#'])
+      console.log("account_> ", response.data.recordset[0]);
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
+  removeAccount(token, tc, additNo){
+    var config = {
+      headers:{'token': "" + token}
+    }
+    var bodyParameters = {
+      tc: parseInt(tc),
+      additNo:parseInt(additNo)
+    }
+    axios.post(this.url+'/api/account/deleteAccount',
+      bodyParameters,
+      config
+    ).then((response) => {
+      console.log("aha res", response.data.status);
+      if(response.data.status == 500){
+        this.alertService.error("Hesapta para var! Hesap kapatma işlemi başarısız!");
+      } else{
+        this.alertService.success("Hesap Kapatma işlemi başarılı!");
+        this.router.navigate(['#'])
+      }
+      console.log("account_> ", response.data);
     }).catch((error) => {
       console.log(error)
     });
