a14e8d291c59f7935755699a5a66fbfef5383d9b
Yusuf Dede <yusufdede95@hotmail.com>
2019-12-16 23:09:36 +0300

    Predict Form input read console

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Rate } from './rate';
 import {HttpClient} from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-homepage',
@@ -9,16 +11,53 @@ import {HttpClient} from '@angular/common/http';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor(private httpClient:HttpClient) { }
+  url:string = 'https://stuxnetapi.herokuapp.com';
+  credit:any;
+  age:any;
+  home:any;
+  creditCount:any;
+  phoneState:any;
+
+  decisionRes:any[] = [];
+
+  predictForm: FormGroup;
+
+  constructor(
+    private httpClient:HttpClient,
+    private formBuilder: FormBuilder,
+    private alertService: ToastrService,
+    ) { }
 
   path:string="https://web-paragaranti-pubsub.foreks.com/web-services/securities/exchanges/BIST/groups/E"
   
   rates:Rate[];
 
   ngOnInit() {
+
+    this.predictForm = this.formBuilder.group({
+      credit: ['', Validators.required],
+      age: ['', Validators.required],
+      home: ['', Validators.required],
+      creditCount: ['', Validators.required],
+      phoneState: ['', Validators.required],
+     });
+
     this.httpClient.get<Rate[]>(this.path).subscribe(response =>{
       this.rates=response;
     })
+  }
+  
+  get f() { return this.predictForm.controls; } 
+
+  onSubmit() {
+    console.log("predict form", 
+      this.f.credit.value, 
+      this.f.age.value, 
+      this.f.home.value,
+      this.f.creditCount.value,
+      this.f.phoneState.value
+      );
+
   }
 
 }
